9e580bc3259ec8dd0c104c3191b847c7d8979c47
Uiseop Eom <ytrtef@gmail.com>
2023-04-07 00:13:39 +0900

    chore: Add fastapi submodule and pytest

---
+++ reference/pytest-tutorial/ch16/test_optional.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.fixture()
+def user(request):
+    role = getattr(request, "param", "visitor")
+    print(f"\nLog in as {role}")
+    return role
+
+
+def test_unspecified_user(user):
+    ...
+
+
+@pytest.mark.parametrize("user", ["admin", "team_member"], indirect=["user"])
+def test_admin_and_team_member(user):
+    ...
---
+++ scripts/append_ref_to_filename.py
@@ -0,0 +1,26 @@
+import os
+import re
+import sys
+
+if sys.argv[1] is None:
+    raise ValueError(
+        "Please provide a path to the directory containing the files to rename."
+    )
+
+# Define the path to the directory containing the files
+target_dir = sys.argv[1]
+suffix = "_ref"
+
+# Compile a regular expression to match file extensions
+ext_pattern = re.compile(r"\.[^.]+$")
+
+for root, dirs, files in os.walk(target_dir):
+    for filename in files:
+        if ext_pattern.search(filename) and not filename.endswith(suffix):
+            old_path = os.path.join(root, filename)
+            ext = ext_pattern.search(old_path).group()
+            new_path = (
+                os.path.join(root, ext_pattern.sub(f"({suffix})\0", filename)) + ext
+            )
+            print(f"Renaming {old_path} to {new_path}...")
+            os.rename(old_path, new_path)
