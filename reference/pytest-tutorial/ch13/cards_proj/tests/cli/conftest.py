e8c957b024728f152144a5b7db6c9cb9431ebd93
Uiseop Eom <ytrtef@gmail.com>
2023-04-07 00:14:12 +0900

    chore: Add fastapi submodule and pytest (#12)

---
+++ reference/pytest-tutorial/ch13/cards_proj/tests/cli/conftest.py
@@ -0,0 +1,24 @@
+import shlex
+
+import cards
+import pytest
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+@pytest.fixture()
+def cards_cli_no_redirect():
+    def run_cli(command_string):
+        command_list = shlex.split(command_string)
+        result = runner.invoke(cards.cli.app, command_list)
+        output = result.stdout.rstrip()
+        return output
+
+    return run_cli
+
+
+@pytest.fixture()
+def cards_cli(cards_cli_no_redirect, db_path, monkeypatch, cards_db):
+    monkeypatch.setenv("CARDS_DB_DIR", db_path.as_posix())
+    return cards_cli_no_redirect
