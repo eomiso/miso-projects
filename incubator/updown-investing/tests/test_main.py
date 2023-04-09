66b7f36c5acb64f769a3fab238230f4cefcc5573
Uiseop Eom <ytrtef@gmail.com>
2023-04-09 23:24:51 +0900

    feat: Dockerfile investigation

--- incubator/updown-investing/tests/test_main.py
+++ incubator/updown-investing/tests/test_main.py
@@ -1,7 +1,8 @@
 import pytest
-from app.main import app
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
+
+from app.main import app
 
 client = TestClient(app)
 
@@ -16,6 +17,6 @@ def test_read_root():
 async def test_async_read_root():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/async-root")
+
         assert response.status_code == 200
-        assert response.json() == {"msg": "Hello World"}
         assert response.json() == {"msg": "Hello World"}
