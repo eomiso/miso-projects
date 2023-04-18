0f3a0dfa92c7acc6ca92dea52e94c83a79a1e441
Uiseop Eom <ytrtef@gmail.com>
2023-04-19 00:34:17 +0900

    Scheduled merge to main (#28)
    * Docker multi-stage build investigation (#25)
    
    * fix: Change name of auto-merge github workflow
    
    * feat: Add docker-tutorial
    
    * feat: add tests
    
    * feat: Dockerfile investigation
    
    * fix: Fix workflow
    
    * feat: Add test workflow (#26)
    
    * pytest tutorial/aesop/fix ref script (#27)
    
    * fix: Fix bug in ref script
    
    * chore: Add ref tag to tutorial code
    
    * fix: Auto merge dev 수정

--- incubator/updown-investing/app/main.py
+++ incubator/updown-investing/app/main.py
@@ -5,4 +5,9 @@ app = fastapi.FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"msg": "Hello World"}
+
+
+@app.get("/async-root")
+async def async_read_root():
+    return {"msg": "Hello World"}
