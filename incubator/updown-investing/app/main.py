651d2b7030203eadbd4cbb91b7a60e83b845d013
Uiseop Eom <ytrtef@gmail.com>
2023-04-19 01:10:20 +0900

    Revert "Scheduled merge to main (#28)" (#32)
    This reverts commit 0f3a0dfa92c7acc6ca92dea52e94c83a79a1e441.

--- incubator/updown-investing/app/main.py
+++ incubator/updown-investing/app/main.py
@@ -5,9 +5,4 @@ app = fastapi.FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"msg": "Hello World"}
-
-
-@app.get("/async-root")
-async def async_read_root():
-    return {"msg": "Hello World"}
+    return {"Hello": "World"}
--- incubator/updown-investing/scripts/test.sh
+++ incubator/updown-investing/scripts/test.sh
@@ -4,5 +4,4 @@ set -e
 set -x
 
 export PYTHONPATH=./app
-coverage run --rcfile ./pyproject.toml -m pytest tests "${@}"
-coverage report --fail-under 95 --show-missing
+coverage run -m pytest tests "${@}"
