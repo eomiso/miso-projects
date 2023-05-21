b394a06854e77cf9cac58345314e0812b2285b7a
Uiseop Eom <ytrtef@gmail.com>
2023-05-21 23:27:03 +0900

    * wig: Trailing commit

--- incubator/node-frontend/scripts/build-push.sh
+++ incubator/node-frontend/scripts/build-push.sh
@@ -8,7 +8,6 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker buildx build --platform=linux/amd64,linux/arm64 -t eomiso/node-frontend:18 .
 
 docker buildx build --platform=linux/amd64,linux/arm64 -t eomiso/node-frontend:latest .
-docker build -t eomiso/node-frontend:latest .
 
 docker push eomiso/node-frontend:18
 
