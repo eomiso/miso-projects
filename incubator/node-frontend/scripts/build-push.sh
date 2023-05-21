9b18849488b225a95bfff784f34356b0023f7517
Uiseop Eom <ytrtef@gmail.com>
2023-05-21 23:39:12 +0900

    feat: Change yaml extensions

--- incubator/node-frontend/scripts/build-push.sh
+++ incubator/node-frontend/scripts/build-push.sh
@@ -5,10 +5,10 @@ set -x
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker buildx build --platform=linux/amd64,linux/arm64 -t eomiso/node-frontend:18 .
+docker buildx build --platform=linux/amd64 -t eomiso/node-frontend:18 .
 
-docker buildx build --platform=linux/amd64,linux/arm64 -t eomiso/node-frontend:latest .
-
-docker push eomiso/node-frontend:18
+docker buildx build --platform=linux/amd64 -t eomiso/node-frontend:latest .
 
 docker push eomiso/node-frontend:18
+
+docker push eomiso/node-frontend:latest
